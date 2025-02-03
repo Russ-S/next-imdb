3196a407439da19705eb6310395c634bf4f8d5f1
Russ-S <hesys@cs.com>
2025-02-03 12:48:30 -0700

    Fix mongodb error

--- src/lib/actions/user.js
+++ src/lib/actions/user.js
@@ -1,10 +1,10 @@
 import User from '../models/user.model'
-import { connect } from '..mongodb/mongoose'
-
+import { connect } from '../mongodb/mongoose'
 export const createOrUpdateUser = async (
   id,
   first_name,
   last_name,
+  image_url,
   email_addresses
 ) => {
   try {
@@ -16,21 +16,21 @@ export const createOrUpdateUser = async (
           firstName: first_name,
           lastName: last_name,
           profilePicture: image_url,
-          email: email_addresses(0).email_address,
+          email: email_addresses[0].email_address,
         },
       },
       { upsert: true, new: true }
     )
     return user
   } catch (error) {
-    console.log('Error: Could not create or update user', error)
+    console.log('Error: Could not create or update user:', error)
   }
 }
-export const deleteUser = async ({ id }) => {
+export const deleteUser = async (id) => {
   try {
     await connect()
-    await user.findOneAnDelete({ clerkId: id })
+    await User.findOneAndDelete({ clerkId: id })
   } catch (error) {
-    console.log('Error: Could not delete user', error)
+    console.log('Error: Could not delete user:', error)
   }
 }
